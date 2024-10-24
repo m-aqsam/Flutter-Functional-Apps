3e1d8be947a39b3a5be80ac3488d3cf380dfdf86
Aqsam <maqsam1155@gmail.com>
2024-10-25 01:10:23 +0500

    reset password

---
+++ 07_Ecommerce_App/e_shop/lib/features/Authentication/password_configuration/reset_password.dart
@@ -0,0 +1,30 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:e_shop/common/styles/spacing_style.dart';
+import 'package:e_shop/utlis/constants/text_strings.dart';
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+
+class Reset_Password extends StatelessWidget {
+  const Reset_Password({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+        padding: ESpacingStyle.paddingWithAppBarHeight,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // ************************* Image *************************
+
+            // ************************* Heading & Subtitle ***********************
+
+            // ************************* Button *************************
+          ],
+        ),
+      ),
+    );
+  }
+}
