93be13250e7e672cc87f9ecb75e46888cd77c3b8
Aqsam <maqsam1155@gmail.com>
2024-10-25 01:10:09 +0500

    new screen

---
+++ 07_Ecommerce_App/e_shop/lib/features/Authentication/password_configuration/forgot_password.dart
@@ -0,0 +1,62 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:e_shop/common/styles/spacing_style.dart';
+import 'package:e_shop/utlis/constants/text_strings.dart';
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+
+class Forgot_Password extends StatelessWidget {
+  const Forgot_Password({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+        padding: ESpacingStyle.paddingWithAppBarHeight,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // ************************* Heading & Subtitle *************************
+
+            Text(
+              ETexts.forgotPasswordTitle,
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+
+            SizedBox(height: 16),
+
+            Text(
+              ETexts.forgotPasswordSubTitle,
+              style: Theme.of(context).textTheme.labelMedium,
+            ),
+
+            SizedBox(height: 26),
+
+            // ************************* Email-Textfield ***********************
+
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: ETexts.loginEmail,
+                hintText: ETexts.loginEmailHint,
+                prefixIcon: Icon(Iconsax.direct_right),
+              ),
+            ),
+
+            SizedBox(height: 20),
+
+            // ************************* Submit Button *************************
+
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text(ETexts.forgotPasswordButton),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
