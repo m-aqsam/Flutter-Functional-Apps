1156c560068a448b2f3bb4f227c842c7eed15d3b
Aqsam <maqsam1155@gmail.com>
2024-10-25 23:52:15 +0500

    update

--- 07_Ecommerce_App/e_shop/lib/main.dart
+++ 07_Ecommerce_App/e_shop/lib/main.dart
@@ -4,7 +4,7 @@ import 'package:e_shop/app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  //WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   //FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
 
   runApp(const App());
