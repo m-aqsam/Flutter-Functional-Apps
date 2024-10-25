003e0ee51e19d7d55c5227c77de713e9c87e758b
Aqsam <maqsam1155@gmail.com>
2024-10-25 23:08:08 +0500

    new update

--- 07_Ecommerce_App/e_shop/lib/features/Authentication/password_configuration/reset_password.dart
+++ 07_Ecommerce_App/e_shop/lib/features/Authentication/password_configuration/reset_password.dart
@@ -1,9 +1,11 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:e_shop/common/styles/spacing_style.dart';
+import 'package:e_shop/features/Authentication/login/ui/screens/login_screen.dart';
+import 'package:e_shop/utlis/constants/image_strings.dart';
 import 'package:e_shop/utlis/constants/text_strings.dart';
+import 'package:e_shop/utlis/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
-import 'package:iconsax/iconsax.dart';
 
 class Reset_Password extends StatelessWidget {
   const Reset_Password({super.key});
@@ -11,17 +13,68 @@ class Reset_Password extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => Login_Screen(),
+                  ));
+            },
+            icon: Icon(Icons.close),
+          ),
+        ],
+      ),
       body: Padding(
         padding: ESpacingStyle.paddingWithAppBarHeight,
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             // ************************* Image *************************
 
-            // ************************* Heading & Subtitle ***********************
+            Image(
+              width: EHelperFunctions.screenWidth() * 0.7,
+              image: AssetImage(EImages.emailLinkSend),
+            ),
 
-            // ************************* Button *************************
+            ///***************  Tittle $ Subtitle ***************
+            Text(
+              ETexts.forgotPasswordEmailSendTittle,
+              style: Theme.of(context).textTheme.headlineMedium,
+              textAlign: TextAlign.center,
+            ),
+
+            SizedBox(height: 16),
+
+            //***************  Description ***************
+
+            Text(
+              ETexts.forgotPasswordEmailSendDescription,
+              style: Theme.of(context).textTheme.labelMedium,
+              textAlign: TextAlign.center,
+            ),
+
+            SizedBox(height: 20),
+
+            //***************  Buttons ****************
+
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text(ETexts.forgotPasswordEmailDone)),
+            ),
+
+            // Email Resend Button
+
+            SizedBox(
+              width: double.infinity,
+              child: TextButton(
+                  onPressed: () {}, child: Text(ETexts.verifyEmailResend)),
+            ),
           ],
         ),
       ),
