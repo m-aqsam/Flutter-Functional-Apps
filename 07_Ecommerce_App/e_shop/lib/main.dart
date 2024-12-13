898a1ab91d49dc7c819380ac2ec1ac042b3ffd7a
Aqsam <maqsam1155@gmail.com>
2024-12-13 23:39:39 +0500

    update

--- 07_Ecommerce_App/e_shop/lib/main.dart
+++ 07_Ecommerce_App/e_shop/lib/main.dart
@@ -4,15 +4,21 @@ import 'package:e_shop/app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 
-void main() {
+void main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
 
+  await Future.delayed(const Duration(seconds: 2));
+
+  FlutterNativeSplash.remove();
+
   runApp(const App());
+}
+
 
   // Todo: Add Widget Binding
   // Todo: Init Local Storage
   // Todo: Await Native Splash Screen
   // Todo: Initialize Firebase
   // Todo: Initialize  Authentication
-}
+
