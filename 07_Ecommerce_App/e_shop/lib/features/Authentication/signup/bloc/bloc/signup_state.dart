f892323eda7305e4c76d47f259e0b88ec1c94d4a
Aqsam <maqsam1155@gmail.com>
2024-12-22 23:58:51 +0500

    Update

--- 07_Ecommerce_App/e_shop/lib/features/Authentication/signup/bloc/bloc/signup_state.dart
+++ 07_Ecommerce_App/e_shop/lib/features/Authentication/signup/bloc/bloc/signup_state.dart
@@ -2,6 +2,8 @@ part of 'signup_bloc.dart';
 
 @immutable
 sealed class SignupState {}
+
 sealed class SignupActionState extends SignupState {}
 
 final class SignupInitial extends SignupState {}
+//
