a4ab5892329da0993e2749e8a6155d98dacfa1a9
Aqsam <maqsam1155@gmail.com>
2024-12-26 23:54:30 +0500

    update

--- 07_Ecommerce_App/e_shop/lib/features/Authentication/signup/ui/screens/signup_screen.dart
+++ 07_Ecommerce_App/e_shop/lib/features/Authentication/signup/ui/screens/signup_screen.dart
@@ -75,6 +75,7 @@ class Signup_Screen extends StatelessWidget {
                   onGooglePressed: () {},
                   onFacebookPressed: () {},
                 ),
+                //
               ],
             ),
           ),
