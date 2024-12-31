c822c8ac8d8d1c8a4eb3a344effcc8d73ac070ba
Aqsam <maqsam1155@gmail.com>
2024-12-31 23:19:45 +0500

    Update

--- 07_Ecommerce_App/e_shop/lib/features/Authentication/password_configuration/reset_password.dart
+++ 07_Ecommerce_App/e_shop/lib/features/Authentication/password_configuration/reset_password.dart
@@ -63,6 +63,7 @@ class Reset_Password extends StatelessWidget {
               child: TextButton(
                   onPressed: () {}, child: Text(ETexts.verifyEmailResend)),
             ),
+            //
           ],
         ),
       ),
