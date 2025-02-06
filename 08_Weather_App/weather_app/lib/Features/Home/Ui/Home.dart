7fa2d1c6dabc0396ee309087eef2ec641cd8c989
Aqsam <maqsam1155@gmail.com>
2025-02-06 23:23:18 +0500

    Update

---
+++ 08_Weather_App/weather_app/lib/Features/Home/Ui/Home.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Home_Page extends StatelessWidget {
+  const Home_Page({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
+      body: const Center(
+        child: Text(
+          'This is the Home Page',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
